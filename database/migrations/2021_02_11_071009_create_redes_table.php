048bb302cf8f26a28098495a0342a80ad961cbc2
juand.maruladnaq <juand.marulandaq@autonoma.edu.co>
2021-02-11 04:41:55 -0500

    se craron: controladores,migraciones,modelos

---
+++ database/migrations/2021_02_11_071009_create_redes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRedesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('redes', function (Blueprint $table) {
+            $table->id();
+            $table->string('label', 128);            
+            $table->string('url', 256);
+            $table->foreignId('user_id');
+            $table->timestamps();
+
+            
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('redes');
+    }
+}
